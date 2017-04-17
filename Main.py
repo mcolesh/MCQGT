8c3937da0fe8ff3526ec5b9159467cb426d442f9
Jonathan <jongit3@gmail.com>
2017-04-17 21:44:15 +0300

    before grid

--- Main.py
+++ Main.py
@@ -1,18 +1,26 @@
 from ast import literal_eval
 from Step1 import Step1
-
+from collections import OrderedDict
+import operator
 
 # input
-gl = literal_eval(input("Please enter the data: "))
-radius = input("Enter the radius: ")
-q0 = input("Enter the genomes quorum: ")
+'''gl = literal_eval(input("Please enter the data: "))
+d = input("Enter the radius: ")
+q0 = input("Enter the genomes quorum: ")'''
 
 # create step 1 object
-step1 = Step1(q0, gl, radius)
-print("hello")
+gl = [("kinase",3),("tf",2)]
+q0 = 1
+d = 1500
+step1 = Step1(gl, q0, d)
 print(step1.get_cogs_frequency())
+print(step1.get_functions_frequency())
+#print(step1.get_grid())
+#print(sorted(step1.get_functions_frequency().items(), key=operator.itemgetter(1)))
+#print(sorted(step1.get_cogs_frequency().items(), key=operator.itemgetter(1)))
 
-# initialize DICTIONARY
+#print(step1.get_functions_frequency())
+#print(step1.get_cogs_frequency())
 
 
 
--- Step1.py
+++ Step1.py
@@ -1,25 +1,31 @@
 import mysql.connector
-
+import operator
+import collections
+import timeit
 
 
 class Step1:
     __q0 = 0
     __gl = None
-    __radius = 0
+    __window_size = 0
     __cog_functions_dictionary = None
     __function_cogs_dictionary = None
     __cogs_frequency = None
     __functions_frequency = None
+    __grid = None
+
     # g1 example [("kinase",3),("tf",2)]
-    def __init__(self,gl,q0, radius):
+    def __init__(self,gl,q0, window_size):
         self.__gl = gl
         self.__q0 = q0
-        self.__radius = radius
+        self.__window_size = window_size
         two_dictionaries = Step1.gen_dictionary(gl)
         self.__cog_functions_dictionary = two_dictionaries[0]
         self.__function_cogs_dictionary = two_dictionaries[1]
         self.__cogs_frequency = Step1.gen_cogs_frequency(self.__cog_functions_dictionary)
-        #self.__functions_frequency = Step1.gen_function_frequency_dictionary()
+        self.__functions_frequency = \
+            Step1.gen_functions_frequency(self.__function_cogs_dictionary, self.__cogs_frequency)
+        # self.__grid = Step1.gen_grid(list(self.__function_cogs_dictionary.items())[0][1], window_size)
 
     def get_q0(self):
         return self.__q0
@@ -33,11 +39,11 @@ class Step1:
     def set_gl(self, gl):
         self.__gl = gl
 
-    def get_radius(self):
-        return self.__radius
+    def get_window_size(self):
+        return self.__window_size
 
-    def set_radius(self, radius):
-        self.__radius = radius
+    def set_window_size(self, window_size):
+        self.__window_size = window_size
 
     def get_cog_functions_dictionary(self):
         return self.__cog_functions_dictionary
@@ -57,8 +63,21 @@ class Step1:
     def set_cogs_frequency(self, cogs_frequency):
         self.__cogs_frequency = cogs_frequency
 
+    def get_functions_frequency(self):
+        return self.__functions_frequency
+
+    def set_functions_frequency(self, functions_frequency):
+        self.__functions_frequency = functions_frequency
+
+    def get_grid(self):
+        return self.__grid
+
+    def set_grid(self, grid):
+        self.__grid = grid
+
     @staticmethod
     def gen_dictionary(gl):
+        print("generating cog-functions dictionary and function-cogs dictionary")
         # initialize dictionary
         d = dict()
         reverse_d = dict()
@@ -81,7 +100,6 @@ class Step1:
                 if ID in d:
                     d[ID].append(c)
                 else:
-                    #This is a bug fix
                     d[ID] = [c]
 
                 if c in reverse_d:
@@ -96,47 +114,263 @@ class Step1:
 
     @staticmethod
     def gen_cogs_frequency(cog_functions_dictionary):
+
+        # create temporary classification-cogs table in the DB
+        Step1.create_classification_cogs_table_in_db(cog_functions_dictionary)
+
+        print("generating cogs frequency dictionary")
+        start_time = timeit.default_timer()
+
         # initialize dictionary
         d = dict()
 
         cogs = cog_functions_dictionary.keys()
 
-        # initialize keys
-        for key in cogs:
-            d[key] = 0
+        # initialize mysql objects
+        cnx = mysql.connector.connect(user='root', database='fullptt_prophageannotation')
+        cursor = cnx.cursor()
 
-        # initialize number of data examples - normalizer
-        normalizer = 6634238
+        #build query
+        query = "SELECT DISTINCT t.cog, c.frequency " \
+                "FROM temp_cogs_classification as t " \
+                "Inner JOIN cogs_frequency as c ON t.cog = c.cog " \
+                "ORDER BY `frequency`"
+
+        # execute query
+        cursor.execute(query)
+
+        # add each result to dictionary
+        for (cog, frequency) in cursor:
+            d[cog] = frequency
+
+        # close mysql objects
+        cursor.close()
+        cnx.close()
+
+        d = collections.OrderedDict(sorted(d.items(), key=operator.itemgetter(1)))
+
+        stop_time = timeit.default_timer()
+
+        print("Took " + str(stop_time - start_time) + " seconds")
+
+        # delete classification-cogs temp table
+        #Step1.mysql_delete_classification_cogs_table()
+        return d
+
+    @staticmethod
+    def create_classification_cogs_table_in_db(cog_functions_dictionary):
+
+        value_pairs = Step1.dict_to_mysql_value_pairs(cog_functions_dictionary)
+
+        cols = ["`cog`", "`classification`"]
+
+        Step1.mysql_create_classification_cogs_table()
+        Step1.mysql_insert_values_classification_cogs_table(cols, value_pairs)
+
+    @staticmethod
+    def mysql_create_classification_cogs_table():
 
         # initialize mysql objects
         cnx = mysql.connector.connect(user='root', database='fullptt_prophageannotation')
         cursor = cnx.cursor()
 
         # build query
-        where = "where"
-        i = 1
-        for cog in cogs:
-            if i == 1:
-                where += " 'cog'='{}' ".format(cog)
-            if i > 1:
-                where += " OR 'cog'='{}' ".format(cog)
-            i += 1
-        query = "select `cog`, COUNT(*) as num from data " + where + "GROUP BY `cog` ORDER BY `num` ASC";
+        query = "CREATE TABLE temp_cogs_classification (cog VARCHAR(7), classification VARCHAR(94))"
 
         # execute query
         cursor.execute(query)
-
-        # add each result to dictionary
-        for (cog, num) in cursor:
-            d[cog] = num
-            print("%{}%, %{}% ".format(cog, num))
 
         # close mysql objects
         cursor.close()
         cnx.close()
 
+    @staticmethod
+    def mysql_insert_values_classification_cogs_table(col_names, value_pairs):
+
+        #
+        # initialize mysql objects
+        cnx = mysql.connector.connect(user='root', database='fullptt_prophageannotation')
+        cursor = cnx.cursor()
+
+        # build query
+        query = "INSERT INTO %s (%s) VALUES %s" % (
+            'temp_cogs_classification', ",".join(col_names), ",".join(value_pairs))
+
+        # execute query
+        cursor.execute(query)
+
+        # close mysql objects
+        cursor.close()
+        cnx.close()
+
+    @staticmethod
+    def mysql_delete_classification_cogs_table():
+
+        # initialize mysql objects
+        cnx = mysql.connector.connect(user='root', database='fullptt_prophageannotation')
+        cursor = cnx.cursor()
+
+        # build query
+        query = "DROP TABLE temp_cogs_classification"
+
+        # execute query
+        cursor.execute(query)
+
+        # close mysql objects
+        cursor.close()
+        cnx.close()
+
+
+    @staticmethod
+    def dict_to_mysql_value_pairs(d):
+        records = []
+
+        # build records of table
+        for key, values in d.items():
+            for value in values:
+                records.append("('{}','{}')".format(key, value))
+
+        return records
+
+
+    @staticmethod
+    def generate_orwhere_of_cogs(cogs):
+        where = "where"
+        i = 1
+        for cog in cogs:
+            if i == 1:
+                where += " `cog`='{}' ".format(cog)
+            if i > 1:
+                where += " OR `cog`='{}' ".format(cog)
+            i += 1
+        return where
+
+    @staticmethod
+    def gen_functions_frequency(functions_cog_dictionary, cogs_frequency_dictionary):
+        print("generating functions frequency dictionary")
+
+        # initialize dictionary
+        d = dict()
+
+        # for every function in functions_cog_dictionary
+        for func in functions_cog_dictionary:
+
+            # get cogs list
+            cog_list = functions_cog_dictionary[func]
+
+            # sum cogs frequency
+            func_frequency = 0
+            for cog in cog_list:
+                func_frequency += cogs_frequency_dictionary[cog]
+
+            # update function frequency
+            d[func] = func_frequency
+
+        d = collections.OrderedDict(sorted(d.items(), key=operator.itemgetter(1)))
+
         return d
 
+    @staticmethod
+    def gen_grid(cog_list, window_size):
+
+        # create temp table to store records (#bateria, loc_start, loc_end, cog) of cogs from GL (denote as t)
+
+        # create nested query in this fashion:
+            # select all records from `data_sorted_by_bac_and_locstart` (denote as s) s.t.:
+                # select all records from t
+            # where s.#bacteria=t.#bacteria AND
+        #       ( s.loc_start >= t.loc_start + window_size - 1 OR s.loc_end <= t.loc_end - window_size + 1)
+
+        # save results as a dictionary with 2 keys (bacteria + loc_start) if such thing is possible in python
+
+        # delete the temp table
+
+        '''
+        
+                print("generating pre-grid")
+        # initialize pregrid (contains just LFF (Lowest Frequency Functionality) COGS)
+        pre_grid = dict()
+
+        # generate orwhere of cogs
+        where = Step1.generate_orwhere_of_cogs(cog_list)
+
+        #build query
+        query = "select `#bacteria`,`cog`,`loc_start`,`loc_end` from data5 " + where + \
+                "ORDER BY `#bacteria`,`loc_start` ASC"
+
+        # initialize mysql objects
+        cnx = mysql.connector.connect(user='root', database='fullptt_prophageannotation')
+        cursor = cnx.cursor()
+
+        # execute query
+        cursor.execute(query)
+
+        # add each result to pregrid
+        for (bac, cog, start, end) in cursor:
+            if bac in pre_grid:
+                pre_grid[bac].append([cog, start, end])
+            else:
+                pre_grid[bac] = [[cog, start, end]]
+
+        # close mysql objects
+
+        cursor.close()
+        cnx.close()
+
+        # generate grid from pre grid
+        grid = Step1.gen_grid_from_pregrid(pre_grid, window_size)
+
+        return grid
+        '''
+
+
+    @staticmethod
+    def gen_grid_from_pregrid(pre_grid, window_size):
+        print("generating grid")
+        # initialize grid
+        grid = dict()
+
+        # generate orwhere of cogs
+        where = Step1.generate_where_of_cogs_surrounding_lff_cogs(pre_grid, window_size)
+
+        #build query
+        query = "select `#bacteria`,`cog`,`loc_start`,`loc_end` from data5 " + where + \
+                "ORDER BY `#bacteria`,`loc_start` ASC"
+
+        # initialize mysql objects
+        cnx = mysql.connector.connect(user='root', database='fullptt_prophageannotation')
+        cursor = cnx.cursor()
+
+        # execute query
+        cursor.execute(query)
+
+        # add each result to dictionary
+        for (bac, cog, start, end) in cursor:
+            if bac in d:
+                grid[bac].append([cog, start, end])
+            else:
+                grid[bac] = [[cog, start, end]]
+
+        # close mysql objects
+        cursor.close()
+        cnx.close()
+
+        return grid
+
+    @staticmethod
+    def generate_where_of_cogs_surrounding_lff_cogs(pre_grid, window_size):
+        where = "where"
+        i = 1
+        for bac, cogs in pre_grid.items():
+            for cog_record in cogs:
+                if i == 1:
+                    where += " (`#bacteria`='{}' AND (`loc_start`>='{}' OR `loc_end`<='{}')) ".\
+                        format(bac, cog_record[1] - window_size + 1, cog_record[2] + window_size - 1)
+                if i > 1:
+                    where += " OR (`#bacteria`='{}' AND (`loc_start`>='{}' OR `loc_end`<='{}')) ". \
+                        format(bac, cog_record[1] - window_size + 1, cog_record[2] + window_size - 1)
+                i += 1
+        return where
 
 
 
